e9005e2a6c58b54dc7085a28c34ab98cb36025f9
Michael Eichenseer <16592938+mymindwentblvnk@users.noreply.github.com>
2022-11-10 14:34:31 +0100

    Introduce Dockerfile.

--- app/main.py
+++ app/main.py
@@ -8,10 +8,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"msg": "Hello World"}
+    return {"msg": "Hello World!"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    print(q)
     return {"item_id": item_id, "q": q}
